c54e5c010dc676a772c4f24619f97a20548b3840
tanjie <zyzz521@gmail.com>
2017-11-20 21:59:38 +0800

    from lab 20171120 21:59

--- app/src/main/java/org/zyzz/myapplication/MainActivity.java
+++ app/src/main/java/org/zyzz/myapplication/MainActivity.java
@@ -1,42 +1,38 @@
 package org.zyzz.myapplication;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
-
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
-    TextView txvB,txvG,txvR;
-    Button changeButton;
+    RadioGroup radioGroup;
+    TextView txv;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = findViewById(R.id.editText);
-        txvR = findViewById(R.id.txvR);
-        txvG = findViewById(R.id.txvG);
-        txvB = findViewById(R.id.txvB);
-        changeButton = findViewById(R.id.button);
+        txv = findViewById(R.id.txv);
+        radioGroup = findViewById(R.id.ticketType);
+        button = findViewById(R.id.btn);
     }
 
-    public void changeColor(View view) {
-        Random random = new Random();
-        int  CR = random.nextInt(255);
-        int CG = random.nextInt(255);
-        int CB = random.nextInt(255);
-        txvR.setText("R:"+CR);
-        txvR.setTextColor(Color.rgb(CR,0,0));
-        txvG.setText("G:" + CG);
-        txvG.setTextColor(Color.rgb(0, CG, 0));
-        txvB.setText("B:" + CB);
-        txvB.setTextColor(Color.rgb(0,0,CB));
-        editText.setBackgroundColor(Color.rgb(CR,CG,CB));
+    public void show(View view) {
+        switch (radioGroup.getCheckedRadioButtonId()) {
+            case R.id.adult:
+                txv.setText("买全票");
+                break;
+            case R.id.child:
+                txv.setText("买半票");
+                break;
+            case R.id.senior:
+                txv.setText("买敬老票");
+                break;
+        }
     }
+
 }
